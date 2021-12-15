b447d532bab78216fa1638da074ffaf8a2ba1ab4
Maria Cardona <mechas8703@hotmail.com>
2021-12-15 16:27:34 -0500

    Weather features

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// Weather
+// const apiKey = "231b0a8738afa7bee37961d0b013c3e7"
+// https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}
+// https://api.openweathermap.org/data/2.5/weather?q=${parsedCityName}&appid=${apiKey}
